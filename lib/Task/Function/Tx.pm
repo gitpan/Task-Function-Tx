fdd97e419dcad443f12616ad5bc243bd12ada524
Steven Haryanto <stevenharyanto@gmail.com>
2012-09-04 02:58:40 +0000

    Import of SHARYANTO/Task-Function-Tx-0.01 from CPAN.
    gitpan-cpan-distribution: Task-Function-Tx
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Task-Function-Tx-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Function/Tx.pm
@@ -0,0 +1,44 @@
+package Task::Function::Tx;
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: Install all modules that provide transactional function
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::Function::Tx - Install all modules that provide transactional function
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+=head1 TASK CONTENTS
+
+=head2 Included modules
+
+=head3 L<File::Trash::Undoable>
+
+=head3 L<Rinci>
+
+=head3 L<Task::Setup>
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
